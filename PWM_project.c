afb52d047e6f2dea3cce3eb3c03e9b6d197e0055
Tran Quoc Dai <tranquocdai2003@gmail.com>
2024-06-20 14:28:21 +0700

    Add EXTI to my PWM project

--- PWM_project.c
+++ PWM_project.c
@@ -42,17 +42,13 @@ void LED_PutNum(uint8_t dv, uint8_t chuc, uint8_t tram)
 	lath_74HC595();	
 }
 
-//-----Configuration of using 4 buttons (4 pins in input mode) and 3 pin in output mode (use IC74HC595)------
-void Buttons_IC74HC595_init (void)
+//-----Configuration of using 3 pin in output mode (use IC74HC595)------
+void IC74HC595_init (void)
 {
-	RCC->APB2ENR |= (1<<2)|(1<<3);//clock for GPIOA,GPIOB
+	RCC->APB2ENR |= (1<<3);//clock for GPIOB
 	
 	GPIOB->CRL = 0x00;//Clear
 	GPIOB-> CRL |= (1<<(4*0)) | (1<<(4*1)) | (1<<(4*3));//Output Push Pull PB0, PB1, PB3
-	
-	GPIOA->CRL = 0x00;//Clear
-	GPIOA->CRL |=(8<<(4*2)) | (8<<(4*3)) | (8<<(4*4)) | (8<<(4*5));  //Input PULL Up/Down PA1,PA2,PA3
-	GPIOA->ODR &= ~(1<<2) | ~(1<<3) | ~(1<<4) | ~(1<<5); //Input PULL down PA1,PA2,PA3
 }
 
 //-----Configuration of Timer 2 for PWM------
@@ -60,6 +56,8 @@ void TIM2_PWM_Init (uint16_t psc, uint16_t arr)
 {
 	RCC->APB1ENR |= (1<<0); //clock for TIM2
 	RCC->APB2ENR |= (1<<2); // clock for GPIOA
+	
+	GPIOA->CRL = 0x00;//Clear
 	GPIOA->CRL |= (0xB<<4);// AF ouput push-pull for PA1
 	
 	TIM2->PSC = psc-1;
@@ -77,57 +75,74 @@ void TIM2_PWM_SetDuty (uint16_t duty)
 {
 	TIM2->CCR2=duty;
 }
+//------------------------EXTI for 4 buttons-------
+void Button_EXTI ()
+{
+	RCC->APB2ENR |= (1<<0) | (1<<2);
+	
+	
+	GPIOA->CRL |=(8<<(4*2)) | (8<<(4*3)) | (8<<(4*4)) | (8<<(4*5));  //Input PULL Up/Down PA1,PA2,PA3
+	GPIOA->ODR &= ~(1<<2) | ~(1<<3) | ~(1<<4) | ~(1<<5); //Input PULL down PA1,PA2,PA3
+	
+	AFIO->EXTICR[0] &= ~(1<<8); //LINE 2
+	AFIO->EXTICR[0] &= ~(1<<12); //LINE 3
+	AFIO->EXTICR[1] &= ~(1<<0); //LINE 4
+	AFIO->EXTICR[1] &= ~(1<<4); //LINE 5
+	
+	EXTI->IMR |= (1<<2) | (1<<3) | (1<<4) | (1<<5);
+	
+	EXTI->RTSR |= (1<<2) | (1<<3) | (1<<4) | (1<<5);
+	EXTI->FTSR &= ~(1<<2) | ~(1<<3) | ~(1<<4) | ~(1<<5);
+	
+	NVIC->ISER[0] |= (1<<8) | (1<<9) | (1<<10) | (1<<23);
+}
+
 
 int main(void)
 {
+	//Configuration for 3 pin output to use IC 74HC595
 	Buttons_IC74HC595_init();
+	
+	//Configuration for Timer 2 to use PWM at pin PA1
 	TIM2_PWM_Init(8,1000);
+	
+	//Configuration for 4 buttons using EXTI
+	Button_EXTI();
+	
 	while (1)
-  	{
-		if (((GPIOA->IDR & (1<<2)) == (1<<2)) && (state_button ==0)) 
-		{
-			state_button =1; 
-			tram++;
-			if(tram>9) tram=0;
-		}
-		else if (((GPIOA->IDR & (1<<2)) == 0) && (state_button ==1))
-		{
-			state_button =0; 
-		}
-		if (((GPIOA->IDR & (1<<3)) == (1<<3)) && (state_button_b ==0)) 
-		{
-			state_button_b =1; 
-			chuc++;
-			if(chuc>9) chuc=0;
-		}
-		else if (((GPIOA->IDR & (1<<3)) == 0) && (state_button_b ==1))
-		{
-			state_button_b =0; 
-		}
-		if (((GPIOA->IDR & (1<<4)) == (1<<4)) && (state_button_c ==0)) 
-		{
-			state_button_c =1;
-			dv++;
-			if(dv>9) dv=0;
-		}
-		else if (((GPIOA->IDR & (1<<4)) == 0) && (state_button_c ==1))
-		{
-			state_button_c =0;
-		}
-		
-			
+  	{ 
 		LED_PutNum(dv,chuc,tram);
-			
-			
-		if (((GPIOA->IDR & (1<<5)) == (1<<5)) && (state_button_PWM ==0)) 
-		{
-			state_button_PWM =1; 
-			D=tram*100+chuc*10+dv;
-			TIM2_PWM_SetDuty(D);
-		}
-		else if (((GPIOA->IDR & (1<<5)) == 0) && (state_button_PWM ==1))
-		{
-			state_button_PWM =0; 
-		}
 	}
+}
+
+//--------Interrupt vector---------
+void EXTI2_IRQHandler (void)
+{
+	tram++;
+	if(tram>9) tram=0;
+	EXTI->PR |= (1<<2);
+}
+
+void EXTI3_IRQHandler (void)
+{
+	chuc++;
+	if(chuc>9) chuc=0;
+	EXTI->PR |= (1<<3);
+}
+
+void EXTI4_IRQHandler (void)
+{
+	dv++;
+	if(dv>9) dv=0;
+	EXTI->PR |= (1<<4);
+}
+
+void EXTI9_5_IRQHandler (void)
+{
+	if (EXTI->PR & (1<<5))
+	{
+		D=tram*100+chuc*10+dv;
+		TIM2_PWM_SetDuty(D);
+	}
+	EXTI->PR |= (1<<5);
 }
